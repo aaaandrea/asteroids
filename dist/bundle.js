13790e625962d8ddabb885780088a4a988e8e395
Andrea del Rio <adelrio@gmail.com>
2017-04-26 14:40:56 -0700

    add package.json and adjust bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -17167,6 +17167,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 
+
 const component = () =>  {
   var element = document.createElement('div');
   element.innerHTML = __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' ');
