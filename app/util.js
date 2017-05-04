e80a40115af8c3fb42f18816984f5c3f2c3a5eba
Andrea del Rio <adelrio@gmail.com>
2017-05-04 15:17:06 -0700

    add randomVector and scale to Utils

--- app/util.js
+++ app/util.js
@@ -2,7 +2,14 @@
 const Util = {
 
   randomeVector(length) {
+    // produce a random vector with the legnth
+    const degree = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(degree), Math.cos(degree)], length);
+  },
 
+  scale(vec, m) {
+    // scale the legth of the vector by the amount m.
+    return [vec[0] * m, vec[1] * m];
   }
 
 };
