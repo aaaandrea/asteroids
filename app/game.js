b53bab3672b907813ad878bc44b5fe16db4366e9
Andrea del Rio <adelrio@gmail.com>
2017-05-04 15:25:48 -0700

    addAsteroids to game

--- app/game.js
+++ app/game.js
@@ -1,4 +1,36 @@
+import Asteroid from './asteroid';
+
+
 // holds collections of objects in game
 // key methods include step() - which calls move() on the collections.
   // checkCollisions() checks colliding objects,
   // and draw() which draws the game.
+const CONSTANTS = {
+  DIM_X: 3,
+  DIM_Y: 3,
+  NUM_ASTEROIDS: 45
+};
+
+
+class Game {
+  constructor() {
+    this.asteroids = [];
+
+    this.addAsteroids();
+  }
+
+  addAsteroids() {
+    for (let i = 0; i < CONSTANTS.NUM_ASTEROIDS; i++) {
+      let asteroid = new Asteroid({ game: this });
+      this.asteroids.push(asteroid);
+    }
+  }
+
+  draw(ctx) {
+
+  }
+
+  moveObjects() {
+
+  }
+}
